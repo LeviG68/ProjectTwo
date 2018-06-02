005cd55652bcba32933845e47b2436bf7d13c54c
levi <glendenning_levi@hotmail.com>
2018-06-01 19:05:39 -0600

    push again

--- public/js/tenant.js
+++ public/js/tenant.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#createTenant").on("submit", createTenantFormSubmit);
-$("#login").on("click", login);
-// var login = $('#loginTenant').on('submit', tenantLogIn);
+  $("#login").on("click", login);
+  // var login = $('#loginTenant').on('submit', tenantLogIn);
 
 
-function createTenantFormSubmit(event) {
+  function createTenantFormSubmit(event) {
     event.preventDefault();
     // Calling the upsertAuthor function and passing in the value of the name input
     createTenant({
-        // tenantId: this.tenantId.val().trim(),
-        firstName: $("#firstName").val().trim(),
-        lastName: $("#lastName").val().trim(),
-        phoneNumber: $("#phone").val().trim(),
-        registeredEmail: $("#userName").val().trim(),
-        password: $("#registerPsw").val().trim(),
-        state: $("#state").val().trim(),
-        zipCode: $("#zipCode").val().trim(),
-        city: $("#city").val().trim(),
-        unit: $("#unit").val().trim(),
-        
-             
+      // tenantId: this.tenantId.val().trim(),
+      firstName: $("#firstName").val().trim(),
+      lastName: $("#lastName").val().trim(),
+      phoneNumber: $("#phone").val().trim(),
+      registeredEmail: $("#userName").val().trim(),
+      password: $("#registerPsw").val().trim(),
+      state: $("#state").val().trim(),
+      zipCode: $("#zipCode").val().trim(),
+      city: $("#city").val().trim(),
+      unit: $("#unit").val().trim(),
+
+
     });
     console.log("*******This works at line 15 in createTicket.js*********");
   }
 
-  
+
 
   function login(event) {
     event.preventDefault();
@@ -33,11 +33,28 @@ function createTenantFormSubmit(event) {
       registeredEmail: $('#logInEmail').val().trim(),
       password: $('#pwd').val().trim(),
     }
-    $.post("/api/login", {email: loginObject.registeredEmail, password: loginObject.password})
-    .then(function(data) {
-      window.location.replace("/ticket");
-    })
+    $.post("/api/login", {
+        email: loginObject.registeredEmail,
+        password: loginObject.password
+      })
+      .then(function (data) {
+        window.location.replace("/ticket");
+      })
   }
+
+  var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+  // var requiresAdmin = function () {
+  //   return [
+  //     ensureLoggedIn('/admin'),
+  //     function (req, res, next) {
+  //       if (req.user && req.user.isTenant === true)
+  //         next();
+  //       else
+  //         res.send(401, 'Unauthorized');
+  //     }
+  //   ]
+  // };
 
   // function tenantLogIn(event) {
   //   console.log('Loging in');
@@ -61,9 +78,8 @@ function createTenantFormSubmit(event) {
 
   // A function for creating an author. Calls getAuthors upon completion
   function createTenant(tenantData) {
-    $.post("/api/tenant", tenantData, function(data) {
+    $.post("/api/tenant", tenantData, function (data) {
       window.location.replace('/ticket');
     });
   }
 });
-
