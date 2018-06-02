9746a7e6fad1bf7699e40396c50b58d39efa366f
Stephanie Glendenning <mtfishergrl@hotmail.com>
2018-06-02 14:22:18 -0600

    added Jaws

--- public/js/createTicket.js
+++ public/js/createTicket.js
@@ -3,7 +3,7 @@ $("#createTicket").on("submit", createTicketFormSubmit);
 function createTicketFormSubmit(event) {
     event.preventDefault();
     createTix({
-        category: $("#categoryOptions > select > option:nth-child(1)").val().trim(),
+        category: $("#categoryOptions > select").val().trim(),
         comment: $("#comment").val().trim() 
     });
     console.log("*******This works at line 15 in createTicket.js*********") 
